fdbdd4ec6d3fb4e52a466eb9f5f0fc6f13d224b6
Ishan Grover <123376573+IshanGrover2004@users.noreply.github.com>
2023-04-25 20:56:08 +0530

    Update TIC-TAC-TOE.cpp

--- TIC-TAC-TOE.cpp
+++ TIC-TAC-TOE.cpp
@@ -2,16 +2,50 @@
 #include <iostream>
 using namespace std;
 
-
+//global variables declaration
 int x,o;
+bool game=true;
+string winner;
+
+//All function used in program
+void welcome();
+void print_board();
+void x_turn();
+void o_turn();
+void result();
+
+
+int main(){
+    welcome();
+    print_board();
+    while(game){
+        x_turn();
+        if(game==false){break;}
+        o_turn();
+    }
+
+    //system("CLS");        //clear the previous outputs
+    return 0;
+}
+
+
+
+void welcome(){
+    cout<<"\n\n\t\t\t\t\t  ***********************************\n";
+    cout<<"\t\t\t\t\t\tT I C  -- T A C -- T O E\t\t\t\n";
+    cout<<"\t\t\t\t\t  ***********************************\n";
+    cout<<"\t\t\t\t\t\t     FOR 2 PLAYERS\n\t\t\t";
+}
+
 
 // Tic-Tac-Toe board structure defining
 char board[9]={'1','2','3','4','5','6','7','8','9'};
 
 void print_board(){
-    string space="\t\t\t\t\t\t  ";
-    string line="\t\t\t\t\t\t_____|_____|_____";
-    string newline="\t\t\t\t\t\t     |     |";
+    
+    string space="      \t\t\t\t\t\t  ";
+    string line="      \t\t\t\t\t\t_____|_____|_____";
+    string newline="      \t\t\t\t\t\t     |     |";
 
     cout<<endl<<endl;
     cout << space << board[0] << "  |  " << board[1] << "  |  " << board[2] <<endl;
@@ -20,7 +54,7 @@ void print_board(){
     cout<< line <<endl<<newline<<endl;
     cout << space << board[6] << "  |  " << board[7] << "  |  " << board[8] <<endl;
     cout<<endl<<endl;
-
+    
 }
 
 
@@ -29,12 +63,14 @@ void x_turn(){
     cout<<"PLAYER 1(X) turn"<<endl;
     cout<<"Print value beetween 1-9: ";
     cin>>x;
-    if(x>9 || x<1){
+    if(x<1 || x>9){
+        cout<<"Write correct value"<<endl;
         x_turn();
     }
     
     //To assign the value of x in board
     if (board[x-1]=='X' || board[x-1]=='O'){        //if given x value is already assigned
+        cout<<"Write correct value"<<endl;
         x_turn();
     }
     else{
@@ -43,6 +79,7 @@ void x_turn(){
 
     //print again board after turn
     print_board();
+    result();
 }
 
 
@@ -52,11 +89,13 @@ void o_turn(){
     cout<<"Print value beetween 1-9: ";
     cin>>o;
     if(o>9 || o<1){
+        cout<<"Write correct value"<<endl;
         o_turn();
     }
     
     //To assign the value of x in board
     if (board[o-1]=='X' || board[o-1]=='O'){        //if given x value is already assigned
+        cout<<"Write correct value"<<endl;
         o_turn();
     }
     else{
@@ -65,55 +104,66 @@ void o_turn(){
 
     //print again board after turn
     print_board();
+    result();
 }
 
 
-string result(){
-    if(board[0]==board[1] && board[1]==board[2]){
+void result(){
+    cout<<endl;
+    if(winner=="X wins" || winner=="O wins" || winner=="Draw"){
+        game=false;
+    }
+
+    if(board[0]==board[1] && board[0]==board[2]){
         (board[0]=='X')?(winner="X wins"):(winner="O wins");
+        game=false;
+        cout<<winner<<endl;
     }
     else if(board[3]==board[4] && board[3]==board[5]){
         (board[3]=='X')?(winner="X wins"):(winner="O wins");
+        game=false;
+        cout<<winner<<endl;
     }
     else if(board[6]==board[7] && board[6]==board[8]){
         (board[6]=='X')?(winner="X wins"):(winner="O wins");
+        game=false;
+        cout<<winner<<endl;
     }
     else if(board[1]==board[4] && board[1]==board[7]){
         (board[1]=='X')?(winner="X wins"):(winner="O wins");
+        game=false;
+        cout<<winner<<endl;
     }
     else if(board[2]==board[5] && board[2]==board[8]){
         (board[2]=='X')?(winner="X wins"):(winner="O wins");
+        game=false;
+        cout<<winner<<endl;
     }
-    else if(board[3]==board[6] && board[3]==board[9]){
+    else if(board[0]==board[3] && board[0]==board[6]){
         (board[3]=='X')?(winner="X wins"):(winner="O wins");
+        game=false;
+        cout<<winner<<endl;
     }
-    else if(board[1]==board[5] && board[1]==board[9]){
+    else if(board[0]==board[4] && board[0]==board[8]){
         (board[1]=='X')?(winner="X wins"):(winner="O wins");
+        game=false;
+        cout<<winner<<endl;
     }
-    else if(board[3]==board[5] && board[3]==board[7]){
+    else if(board[2]==board[4] && board[2]==board[6]){
         (board[3]=='X')?(winner="X wins"):(winner="O wins");
+        game=false;
+        cout<<winner<<endl;
+    }
+    else if(board[0]='1' && board[1]!='2' && board[2]!='3' && board[3]!='4' && board[4]!='5' && board[5]!='6' && board[6]!='7' && board[7]!='8' && board[8]!='9'){
+        game=false;
+        winner="Draw";
+        cout<<winner<<endl;
     }
     else{
-        winner="Draw";
+        game=true;
     }
-    return winner;
-}
-
-
-int main(){
-    cout<<"\n\n\t\t\t  ***********************************\n";
-    cout<<"\t\t\t\tT I C  -- T A C -- T O E\t\t\t\n";
-    cout<<"\t\t\t  ***********************************\n";
-    cout<<"\t\t\t\t     FOR 2 PLAYERS\n\t\t\t";
     
-    // print_board();
-    // x_turn();
-    // o_turn();
-    // x_turn();
-    // o_turn();
-    // x_turn();
-    // o_turn();
-    // cout<<result();
-    // result();
-    return 0;
+    if(winner=="X wins" || winner=="O wins" || winner=="Draw"){
+        game=false;
+    }
 }
